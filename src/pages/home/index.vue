5aa51557758ad0ad0e6e13d1f86677465c1df39e
wangkai <21119441@qq.com>
2018-08-07 10:21:43 +0800

    添加products栏和页面footer脚部

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -162,8 +162,245 @@
           </div>
         </div>
         <!--about end-->
+        <!--product start-->
+        <div class="product wd">
+          <div class="product-title">
+            <h1>PRODUCTS</h1>
+            <a href="/3/"
+              title=""
+              class="more1"></a>
+          </div>
+          <div class="product-con">
+            <a href="javascript:void(0)"
+              title=""
+              class="jt-left1"></a>
+            <a href="javascript:void(0)"
+              title=""
+              class="jt-right1"></a>
+            <div id="pro-con">
+              <ul class="pro-list"
+                id="pro-list">
+                <li>
+                  <a href="/39/42.html"
+                    title="solar power bank"
+                    class="pro-img"><img src="../../images/userfiles/product/solor5LED/00.jpg"
+                      width="301"
+                      height="301"
+                      alt="solar power bank" /></a>
+                  <a href="/39/42.html"
+                    title="solar power bank">solar power bank</a>
+                </li>
+                <li>
+                  <a href="/13/23.html"
+                    title="LED built-in power"
+                    class="pro-img"><img src="../../images/userfiles/product/list/pro1.jpg"
+                      width="301"
+                      height="301"
+                      alt="LED built-in power" /></a>
+                  <a href="/13/23.html"
+                    title="LED built-in power">LED built-in power</a>
+                </li>
+                <li>
+                  <a href="/13/21.html"
+                    title="LED built-in power"
+                    class="pro-img"><img src="../../images/userfiles/product/list/pro8.jpg"
+                      width="301"
+                      height="301"
+                      alt="LED built-in power" /></a>
+                  <a href="/13/21.html"
+                    title="LED built-in power">LED built-in power</a>
+                </li>
+                <li>
+                  <a href="/13/19.html"
+                    title="LED built-in power"
+                    class="pro-img"><img src="../../images/userfiles/product/list/pro7.jpg"
+                      width="301"
+                      height="301"
+                      alt="LED built-in power" /></a>
+                  <a href="/13/19.html"
+                    title="LED built-in power">LED built-in power</a>
+                </li>
+                <li>
+                  <a href="/13/7.html"
+                    title="Fluorescent Power"
+                    class="pro-img"><img src="../../images/userfiles/product/list/pro6.jpg"
+                      width="301"
+                      height="301"
+                      alt="Fluorescent Power" /></a>
+                  <a href="/13/7.html"
+                    title="Fluorescent Power">Fluorescent Power</a>
+                </li>
+                <li>
+                  <a href="/13/1.html"
+                    title="LED built-in power"
+                    class="pro-img"><img src="../../images/userfiles/product/list/pro5.jpg"
+                      width="301"
+                      height="301"
+                      alt="LED built-in power" /></a>
+                  <a href="/13/1.html"
+                    title="LED built-in power">LED built-in power</a>
+                </li>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!--product end-->
       </div>
     </div>
+    <!--footer start-->
+    <div class="footer">
+      <div class="wd">
+        <!--footer top start-->
+        <div class="footer-top">
+          <!--botnav start-->
+          <div class="botnav">
+            <table>
+              <tr class="tr-title">
+                <td>About Us</td>
+                <td>Products</td>
+                <td>News</td>
+                <td>Contact Us</td>
+              </tr>
+              <tr>
+                <td style="width:20%">
+                  <ul>
+                    <li>
+                      <a href="/69/"
+                        title="Company Profile">Company Profile</a>
+                    </li>
+                    <li>
+                      <a href="/217/"
+                        title="Corporate Culture">Corporate Culture</a>
+                    </li>
+                    <li>
+                      <a href="/81/"
+                        title="Honors">Honors</a>
+                    </li>
+                    <li>
+                      <a href="/137/"
+                        title="Office environment">Office environment</a>
+                    </li>
+
+                  </ul>
+                </td>
+                <td style="width:25%">
+                  <ul>
+                    <li>
+                      <a href="/13/"
+                        title="18650  Lithium Battery Power Bank">18650 Lithium Battery Power Bank</a>
+                    </li>
+                    <li>
+                      <a href="/31/"
+                        title="Li-Polymer Battery">Li-Polymer Battery</a>
+                    </li>
+                    <li>
+                      <a href="/39/"
+                        title="Solar Power Bank">Solar Power Bank</a>
+                    </li>
+
+                  </ul>
+                </td>
+                <td style="width:15%">
+                  <ul>
+                    <li>
+                      <a href="/93/"
+                        title="Company News">Company News</a>
+                    </li>
+                    <li>
+                      <a href="/95/"
+                        title="Industry News">Industry News</a>
+                    </li>
+                    <li>
+                      <a href="/155/"
+                        title="Exhibition News">Exhibition News</a>
+                    </li>
+
+                  </ul>
+                </td>
+                <td style="padding-left:15px; width:40%;line-height:25px;">
+                  <p>Contacter: Erin Lee<br /> Title: Sales Maneger<br /> Tel: + 86 755 29606763 <br /> Fax: + 86 755 29604250<br /> Skype: Erin.limeie<br /> Email:
+                    <a href="mailto:judith@szdqdz.com">judith@szdqdz.com</a> <br /> Company: ShenZhen De Qiang Electronics Co., Ltd.<br /> Address: 2F, Block A, Shang Fang Industrial Park, Xin Fa Industrial Zone, Xin Qiao Community,Sha Jing Street, Bao&rsquo;AN District, Shenzhen, Guang Dong Province, China 518104 <br /> &nbsp;
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </div>
+          <!--botnav end-->
+          <!--erwei start-->
+          <div class="erwei"><img src="../../images/erwei.jpg"
+              width="116"
+              height="116"
+              alt="" /></div>
+          <!--erwei end-->
+        </div>
+        <!--footer top end-->
+        <!--live start-->
+        <div class="live">
+          <a href=""
+            title=""><img src="../../images/wx.jpg"
+              width="38"
+              height="34"
+              alt="" /></a>&nbsp;&nbsp;&nbsp;
+          <a href="tencent://message/?uin=&Site=2851080500&Menu=yes"
+            title=""><img src="../../images/qq.jpg"
+              width="38"
+              height="34"
+              alt="" /></a>&nbsp;&nbsp;&nbsp;
+          <a href="http://amos.us.alitalk.alibaba.com/msg.aw?v=2&uid=&site=enaliint&s=5"
+            title=""><img src="../../images/ww.jpg"
+              width="38"
+              height="34"
+              alt="" /></a>&nbsp;&nbsp;&nbsp;
+          <a href="skype:?call"
+            title=""><img src="../../images/skype.jpg"
+              width="38"
+              height="34"
+              alt="" /></a>
+        </div>
+        <!--live end-->
+        <!--bottom start-->
+        <div class="bottom">
+          <div class="bottom-left">
+            <a href="/268/"
+              title="Privacy Policy">Privacy Policy</a> |
+            <a href="/269/"
+              title="Legal Notices">Legal Notices</a> |
+            <a href="sitemap.xml"
+              title="Site Map"
+              target="_blank">Site Map</a><br /> Copyright &copy; 2015 Shenzhen De Qiang Electronics Co. Ltd.
+          </div>
+          <div class="bottom-right">
+            <a href=""
+              title=""
+              target="_blank"><img src="../../images/twinter.jpg"
+                width="33"
+                height="34"
+                alt="" /></a>&nbsp;&nbsp;&nbsp;
+            <a href=""
+              title=""
+              target="_blank"><img src="../../images/face.jpg"
+                width="33"
+                height="34"
+                alt="" /></a>&nbsp;&nbsp;&nbsp;
+            <a href=""
+              title=""
+              target="_blank"><img src="../../images/pink.jpg"
+                width="33"
+                height="34"
+                alt="" /></a>&nbsp;&nbsp;&nbsp;
+            <a href=""
+              title=""
+              target="_blank"><img src="../../images/in.jpg"
+                width="33"
+                height="34"
+                alt="" /></a>
+          </div>
+        </div>
+        <!--bottom end-->
+      </div>
+    </div>
+    <!--footer end-->
   </div>
 </template>
 
@@ -375,8 +612,355 @@ export default {
   float: right;
 }
 
-#pro-con{float:left;width:944px;height:338px;}
-.pro-list{clear:both;}
-.pro-list li{float:left;width:303px;height:338px; text-align:center;line-height:30px;margin-right:15px;font-size:12px;}
-.pro-list li .pro-img{display:block; overflow:hidden;width:301px;height:301px;border:1px solid #BEBEBE;}
+#pro-con {
+  float: left;
+  width: 944px;
+  height: 338px;
+}
+.pro-list {
+  clear: both;
+}
+.pro-list li {
+  box-sizing: border-box;
+  float: left;
+  width: 299px;
+  height: 338px;
+  text-align: center;
+  line-height: 30px;
+  margin-right: 15px;
+  font-size: 12px;
+}
+.pro-list li .pro-img {
+  display: block;
+  overflow: hidden;
+  width: 301px;
+  height: 301px;
+  border: 1px solid #bebebe;
+}
+
+/*============================================footer============================================*/
+.footer {
+  clear: both;
+  margin-top: 10px;
+  background: #8e8e8e url(../../images/footer_bg.jpg) repeat-x;
+  font-family: Times New Roman;
+}
+.footer-top {
+  margin-top: 20px;
+}
+.botnav {
+  float: left;
+  width: 872px;
+  color: #fff;
+}
+.botnav table {
+  width: 100%;
+}
+.botnav .tr-title {
+  height: 30px;
+  line-height: 30px;
+  font-size: 18px;
+}
+.botnav td {
+  vertical-align: top;
+}
+.botnav .tr-title td {
+  background: url(../../images/gray_fk.png) no-repeat left center;
+  padding-left: 15px;
+}
+.botnav ul {
+  clear: both;
+}
+.botnav ul li {
+  height: 30px;
+  line-height: 30px;
+  background: url(../../images/dot.png) no-repeat left center;
+  padding-left: 15px;
+}
+.botnav ul li a {
+  color: #fff;
+}
+
+.erwei {
+  float: right;
+  width: 116px;
+}
+
+.live {
+  clear: both;
+  height: 34px;
+  padding-bottom: 13px;
+  border-bottom: 1px solid #6c6c6c;
+}
+.bottom {
+  clear: both;
+  padding-top: 14px;
+  border-top: 1px solid #b1b1b1;
+  color: #fff;
+  padding-bottom: 10px;
+}
+.bottom .bottom-left {
+  float: left;
+  width: 665px;
+}
+.bottom .bottom-left a {
+  color: #fff;
+  font-weight: bold;
+}
+.bottom .bottom-right {
+  float: right;
+  width: 250px;
+  margin-top: 5px;
+  text-align: right;
+}
+/*==========================================左边===================================================*/
+.left {
+  float: left;
+  width: 225px;
+}
+// .left .left-top{clear:both;height:34px;line-height:34px;background:url(../../images/left_top.jpg) no-repeat;color:#FFF;font-size:14px;padding-left:25px;font-weight:bold;}
+.left .left-con {
+  clear: both;
+  border: 1px solid #cfcfcf;
+  background-color: #f6f6f6;
+  line-height: 30px;
+}
+.left-menu {
+  clear: both;
+}
+.left-menu li {
+  clear: both;
+}
+// .left-menu li a{display:block; overflow:hidden;width:198px;height:30px;background:url(../../images/blue_jt.png) no-repeat 10px center;padding-left:25px;border-bottom:1px solid #E1E1E1;}
+// .left-menu li a:hover{color:#015294;font-weight:bold;background:#C4C4C4 url(../../images/blue_jt.png) no-repeat 10px center;}
+.left-menu li ul {
+  clear: both;
+}
+.left-menu li ul li {
+  clear: both;
+}
+.left-menu li ul li a {
+  background: url(../../images/dot.png) no-repeat 10px center;
+  border-bottom: 1px dotted #e1e1e1;
+}
+.left-menu li ul li a:hover {
+  color: #015294;
+  font-weight: bold;
+  background: url(../../images/dot.png) no-repeat 10px center;
+}
+
+// #sel01{color:#015294;font-weight:bold;background:#C4C4C4 url(../../images/blue_jt.png) no-repeat 10px center;}
+#sel02 {
+  color: #015294;
+  font-weight: bold;
+  background: url(../../images/dot.png) no-repeat 10px center;
+}
+
+.left-contact {
+  padding: 10px;
+  line-height: 25px;
+}
+/*==========================================右边==========================================*/
+.right {
+  float: right;
+  width: 761px;
+  border: 1px solid #cfcfcf;
+}
+// .right .right-top{clear:both;height:33px;background:url(../../images/right_top.jpg) repeat-x;line-height:33px;}
+.right .right-top h1 {
+  float: left;
+  width: 300px;
+  margin-left: 10px;
+  font-size: 14px;
+  color: #015294;
+}
+.right .right-top span {
+  float: right;
+  text-align: right;
+  width: 303px;
+  margin-right: 10px;
+}
+.right .right-con {
+  clear: both;
+  padding: 10px;
+}
+
+.prolist {
+  clear: both;
+  width: 781px;
+}
+.prolist li {
+  width: 202px;
+  height: 232px;
+  line-height: 30px;
+  text-align: center;
+  float: left;
+  margin-right: 45px;
+  margin-top: 10px;
+  margin-left: 10px;
+}
+.prolist li .pro-img {
+  display: block;
+  overflow: hidden;
+  width: 200px;
+  height: 200px;
+  border: 1px solid #e1e1e1;
+}
+.prolist li .pro-img:hover {
+  border-color: #69aed4;
+}
+/***********************新闻列表***********************************/
+.newslist li {
+  height: 35px;
+  line-height: 35px;
+  border-bottom: 1px dashed #cccccc;
+}
+.newslist li a {
+  display: block;
+  height: 35px;
+  line-height: 35px;
+  width: 600px;
+  float: left;
+  padding-left: 10px;
+}
+.newslist li span {
+  display: block;
+  height: 35px;
+  line-height: 35px;
+  width: 80px;
+  float: right;
+}
+/***************新闻详细*********************************/
+.new_title {
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+  color: #f00;
+}
+.new_share {
+  height: 30px;
+  line-height: 30px;
+  clear: both;
+  background-color: #f4f4f4;
+  padding-left: 10px;
+  margin-bttom: 10px;
+  border: 1px dashed #ccc;
+}
+.new_share .publish {
+  text-align: center;
+}
+.new_title1 {
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  font-size: 12px;
+  font-weight: bold;
+  color: #5e8a00;
+}
+.news_img {
+  height: 225px;
+  clear: both;
+  text-align: center;
+}
+.news_img img {
+  border: 1px solid #ccc;
+}
+
+// .prev_n{line-height:30px;padding-left:20px;background-color:#f4f4f4;margin-top:10px;height:60px;border:1px dashed #CCC;}
+// .prev_n .nleft{width:600px;float:left;}
+// .prev_n .goback{width:50px;float:right;background:url(../../images/blue.png) no-repeat left center;padding-left:20px;margin-top:13px;}
+/*********************注册会员*************************/
+.feedback {
+  line-height: 35px;
+  width: 95%;
+  margin-left: 20px;
+  border-left: 1px dashed #ccc;
+  border-top: 1px dashed #ccc;
+}
+.feedback td {
+  border-bottom: 1px dashed #ccc;
+  padding-left: 10px;
+  border-right: 1px dashed #ccc;
+}
+.feedback .tdleft {
+  text-align: right;
+  background-color: #f5f5f5;
+}
+
+.feedback_1 {
+  width: 95%;
+  border: 1px dashed #ccc;
+  background-color: #f5f5f5;
+  margin-top: 30px;
+  margin-left: 20px;
+  line-height: 25px;
+  padding: 10px;
+}
+/*====================资质证书=========================*/
+.hd_list_1 {
+  width: 770px;
+}
+.hd_list_1 li {
+  float: left;
+  width: 156px;
+  height: 243px;
+  line-height: 25px;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 10px;
+}
+.hd_list_1 li img {
+  width: 150px;
+  height: 213px;
+}
+.hd_list_1 li .cer_img {
+  clear: both;
+  border: 1px solid #dbdbdb;
+  display: block;
+  overflow: hidden;
+  width: 150px;
+  height: 213px;
+  padding: 2px;
+  background-color: #fff;
+}
+/*====================资质证书=========================*/
+.hd_list {
+  width: 770px;
+}
+.hd_list_1 li {
+  float: left;
+  width: 156px;
+  height: 243px;
+  line-height: 25px;
+  margin-right: 16px;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 12px;
+}
+.hd_list li {
+  float: left;
+  width: 166px;
+  height: 153px;
+  line-height: 25px;
+  margin-right: 10px;
+  text-align: center;
+  margin-top: 10px;
+  margin-left: 8px;
+}
+.hd_list li img {
+  width: 160px;
+  height: 119px;
+}
+.hd_list li .img {
+  clear: both;
+  border: 1px solid #dbdbdb;
+  display: block;
+  overflow: hidden;
+  width: 160px;
+  height: 119px;
+  padding: 2px;
+  background-color: #fff;
+}
 </style>
