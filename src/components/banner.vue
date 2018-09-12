ac903b5322fc7d6d592bab58613bbafb07be92ab
wangkai <21119441@qq.com>
2018-09-12 14:10:50 +0800

    增加banner的轮播点

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -12,6 +12,12 @@
         <a href="javascript:;"><img src="../images/userfiles/ad/banner4.jpg"
             alt=""></a>
       </div>
+      <div class="bannerDot">
+        <div class="dot active"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,7 +42,8 @@ export default {
   clear: both;
   height: 426px;
   margin-top: 1px;
-  .bannerwrap{  
+  position: relative;
+  .bannerwrap {
     display: flex;
     flex-direction: row;
     justify-content: flex-start;
@@ -46,5 +53,34 @@ export default {
   clear: both;
   height: 230px;
   margin-top: 1px;
+}
+.bannerDot {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  margin: 0;
+  padding: 0;
+  .dot {
+    width: 27px;
+    height: 5px;
+    line-height: 14px;
+    font-size: 12px;
+    background: #cacaca;
+    border: 0px solid #fff;
+    color: #5a5a5a;
+    text-align: center;
+    margin-right: 10px;
+    font-family: Verdana;
+    float: left;
+    cursor: pointer;
+    -webkit-text-size-adjust: none;
+  }
+  .dot.active {
+    background: #005ba6;
+    border: 0px solid #fff;
+    color: #fff;
+    cursor: default;
+  }
 }
 </style>
