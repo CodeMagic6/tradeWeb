85c760c5e1eab98cb7bcd74e72101f24caa73b54
wangkai <21119441@qq.com>
2018-09-12 14:20:38 +0800

    将页面脚部拆分为组件

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -220,159 +220,7 @@
         <!--product end-->
       </div>
     </div>
-    <!--footer start-->
-    <div class="footer">
-      <div class="wd">
-        <!--footer top start-->
-        <div class="footer-top">
-          <!--botnav start-->
-          <div class="botnav">
-            <table>
-              <tr class="tr-title">
-                <td>About Us</td>
-                <td>Products</td>
-                <td>News</td>
-                <td>Contact Us</td>
-              </tr>
-              <tr>
-                <td style="width:20%">
-                  <ul>
-                    <li>
-                      <a href="/69/"
-                        title="Company Profile">Company Profile</a>
-                    </li>
-                    <li>
-                      <a href="/217/"
-                        title="Corporate Culture">Corporate Culture</a>
-                    </li>
-                    <li>
-                      <a href="/81/"
-                        title="Honors">Honors</a>
-                    </li>
-                    <li>
-                      <a href="/137/"
-                        title="Office environment">Office environment</a>
-                    </li>
-
-                  </ul>
-                </td>
-                <td style="width:25%">
-                  <ul>
-                    <li>
-                      <a href="/13/"
-                        title="18650  Lithium Battery Power Bank">18650 Lithium Battery Power Bank</a>
-                    </li>
-                    <li>
-                      <a href="/31/"
-                        title="Li-Polymer Battery">Li-Polymer Battery</a>
-                    </li>
-                    <li>
-                      <a href="/39/"
-                        title="Solar Power Bank">Solar Power Bank</a>
-                    </li>
-
-                  </ul>
-                </td>
-                <td style="width:15%">
-                  <ul>
-                    <li>
-                      <a href="/93/"
-                        title="Company News">Company News</a>
-                    </li>
-                    <li>
-                      <a href="/95/"
-                        title="Industry News">Industry News</a>
-                    </li>
-                    <li>
-                      <a href="/155/"
-                        title="Exhibition News">Exhibition News</a>
-                    </li>
-
-                  </ul>
-                </td>
-                <td style="padding-left:15px; width:40%;line-height:25px;">
-                  <p>Contacter: Erin Lee<br /> Title: Sales Maneger<br /> Tel: + 86 755 29606763 <br /> Fax: + 86 755 29604250<br /> Skype: Erin.limeie<br /> Email:
-                    <a href="mailto:judith@szdqdz.com">judith@szdqdz.com</a> <br /> Company: ShenZhen De Qiang Electronics Co., Ltd.<br /> Address: 2F, Block A, Shang Fang Industrial Park, Xin Fa Industrial Zone, Xin Qiao Community,Sha Jing Street, Bao&rsquo;AN District, Shenzhen, Guang Dong Province, China 518104 <br /> &nbsp;
-                  </p>
-                </td>
-              </tr>
-            </table>
-          </div>
-          <!--botnav end-->
-          <!--erwei start-->
-          <div class="erwei"><img src="../../images/erwei.jpg"
-              width="116"
-              height="116"
-              alt="" /></div>
-          <!--erwei end-->
-        </div>
-        <!--footer top end-->
-        <!--live start-->
-        <div class="live">
-          <a href=""
-            title=""><img src="../../images/wx.jpg"
-              width="38"
-              height="34"
-              alt="" /></a>&nbsp;&nbsp;&nbsp;
-          <a href="tencent://message/?uin=&Site=2851080500&Menu=yes"
-            title=""><img src="../../images/qq.jpg"
-              width="38"
-              height="34"
-              alt="" /></a>&nbsp;&nbsp;&nbsp;
-          <a href="http://amos.us.alitalk.alibaba.com/msg.aw?v=2&uid=&site=enaliint&s=5"
-            title=""><img src="../../images/ww.jpg"
-              width="38"
-              height="34"
-              alt="" /></a>&nbsp;&nbsp;&nbsp;
-          <a href="skype:?call"
-            title=""><img src="../../images/skype.jpg"
-              width="38"
-              height="34"
-              alt="" /></a>
-        </div>
-        <!--live end-->
-        <!--bottom start-->
-        <div class="bottom">
-          <div class="bottom-left">
-            <a href="/268/"
-              title="Privacy Policy">Privacy Policy</a> |
-            <a href="/269/"
-              title="Legal Notices">Legal Notices</a> |
-            <a href="sitemap.xml"
-              title="Site Map"
-              target="_blank">Site Map</a><br /> Copyright &copy; 2015 Shenzhen De Qiang Electronics Co. Ltd.
-          </div>
-          <div class="bottom-right">
-            <a href=""
-              title=""
-              target="_blank"><img src="../../images/twinter.jpg"
-                width="33"
-                height="34"
-                alt="" /></a>&nbsp;&nbsp;&nbsp;
-            <a href=""
-              title=""
-              target="_blank"><img src="../../images/face.jpg"
-                width="33"
-                height="34"
-                alt="" /></a>&nbsp;&nbsp;&nbsp;
-            <a href=""
-              title=""
-              target="_blank"><img src="../../images/pink.jpg"
-                width="33"
-                height="34"
-                alt="" /></a>&nbsp;&nbsp;&nbsp;
-            <a href=""
-              title=""
-              target="_blank"><img src="../../images/in.jpg"
-                width="33"
-                height="34"
-                alt="" /></a>
-          </div>
-        </div>
-        <!--bottom end-->
-      </div>
-    </div>
-    <!--footer end-->
+    <tradefooter></tradefooter>
   </div>
 </template>
 
@@ -380,11 +228,13 @@
 import tradeheader from '../../components/tradeheader';
 import banner from '../../components/banner';
 import notice from '../../components/notice';
+import tradefooter from '../../components/tradefooter';
 export default {
   components: {
     tradeheader,
     banner,
-    notice
+    notice,
+    tradefooter
   },
   data() {
     return {};
@@ -576,80 +426,7 @@ export default {
 }
 
 /*============================================footer============================================*/
-.footer {
-  clear: both;
-  margin-top: 10px;
-  background: #8e8e8e url(../../images/footer_bg.jpg) repeat-x;
-  font-family: Times New Roman;
-}
-.footer-top {
-  margin-top: 20px;
-}
-.botnav {
-  float: left;
-  width: 872px;
-  color: #fff;
-}
-.botnav table {
-  width: 100%;
-}
-.botnav .tr-title {
-  height: 30px;
-  line-height: 30px;
-  font-size: 18px;
-}
-.botnav td {
-  vertical-align: top;
-}
-.botnav .tr-title td {
-  background: url(../../images/gray_fk.png) no-repeat left center;
-  padding-left: 15px;
-}
-.botnav ul {
-  clear: both;
-}
-.botnav ul li {
-  height: 30px;
-  line-height: 30px;
-  background: url(../../images/dot.png) no-repeat left center;
-  padding-left: 15px;
-}
-.botnav ul li a {
-  color: #fff;
-}
 
-.erwei {
-  float: right;
-  width: 116px;
-}
-
-.live {
-  clear: both;
-  height: 34px;
-  padding-bottom: 13px;
-  border-bottom: 1px solid #6c6c6c;
-}
-.bottom {
-  clear: both;
-  padding-top: 14px;
-  border-top: 1px solid #b1b1b1;
-  color: #fff;
-  padding-bottom: 10px;
-}
-.bottom .bottom-left {
-  float: left;
-  width: 665px;
-}
-.bottom .bottom-left a {
-  color: #fff;
-  font-weight: bold;
-}
-.bottom .bottom-right {
-  float: right;
-  width: 250px;
-  margin-top: 5px;
-  text-align: right;
-}
 /*==========================================左边===================================================*/
 .left {
   float: left;
